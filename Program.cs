b19f1a2ac2bb0a2a21aff039616b3e531dbd5a97
David Heizer <DavidHeizer@QuickenLoans.com>
2020-07-09 00:33:27 -0400

    Test data mode made, need to work on if statement validations

--- Program.cs
+++ Program.cs
@@ -14,73 +14,40 @@ namespace Regexp
 
         static void Main(string[] args)
         {
-                    
-            //Console.WriteLine("Hello World!");
-            // @ before "" = able to make \ as a ignore next charater called a literal as a special charater
-            // ^ = Start of String
-            // . = wildcard
-            // * = any number of previous charater
-            // $ = end of String
-            // [abc] = match the char to only a,b, or c
-            // [^abc] = match the char to any except a,b or c
-
-            
             string choice;
+            string cont;
 
-            string[] testdata =
+            do
             {
-                "",
-                "Chuck@TechU..om",
-                "David@Quick@enLoans.com",
-                "Jeff@Grandcircus.com",
-                "madeup@someplace.ca",
-                "fake%new.com",
-                "Jim@Desktop.org", 
-                "@place.com", 
-                "Bob@Burgerscom", 
-                "Keith@Voltron.defender",
-                "David",
-                "Abcdefghijklmn Opqrstuvwxyzextrastuff",
-                "That Guy",
-                "N0t Name",
-                "1/12/2020",
-                "12/31/1999",
-                "15/12/1986",
-                "(1d6)-456-7890",
-                "(123)-456-7890",
-                "1-1-1",
-                "(1234)56-7890",
-                "1&3-456-7890",
-                "9999999999",
-                "123.456.7890",
-                "123-456-7890"
-            };
+                choice = Welcome();
+                switch (choice)
+                {
+                    case "N":
+                        NameVaildation();
+                        break;
+                    case "E":
+                        EmailValidation();
+                        break;
+                    case "P":
+                        PhoneValidation();
+                        break;
+                    case "D":
+                        DateValidation();
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong: A charater other than N, E, P, or D was passed to the switch sstatement");
+                        break;
+                }
+                Console.WriteLine();
+                do
+                {
+
+                    Console.Write("Continue y/n: ");
+                    cont = Console.ReadLine();
+                    cont = cont.ToUpper();
+                } while (cont != "Y" && cont != "N");
+            } while (cont != "N");
             
-
-            choice = Welcome();
-            switch (choice)
-            {
-                case "N":
-                    NameVaildation(testdata);
-                    break;
-                case "E":
-                    EmailValidation(testdata);
-                    break;
-                case "P":
-                    PhoneValidation(testdata);
-                    break;
-                case "D":
-                    DateValidation(testdata);
-                    break;
-                default:
-                    Console.WriteLine("Something went wrong: A charater other than N, E, P, or D was passed to the switch sstatement");
-                    break;
-            }
-
-                               
-
-            
-
         }
 
         static string Welcome()
@@ -114,10 +81,9 @@ namespace Regexp
 //                Console.ReadLine();
             } while (!EntryValid);
             return Entry;
-            
         }
 
-        static void EmailValidation(string[] data)
+        static void EmailValidation()
         {
             Console.Clear();
             string input;
@@ -125,13 +91,16 @@ namespace Regexp
             Regex reg = new Regex(@"^\w{5,30}@\w{5,15}\.\w{2,3}$", RegexOptions.IgnoreCase);
             // Changed the brief becuase @QuickenLoans is 12 charaters 
             // brief said more than 10 in this area is not a valid e-mail address changed to 15
-            
+            // assuming my most likely client would want thier e-mail addresses to validate
+            // *** Need to ask the business if this is acceptable ***
+
+            Console.WriteLine("You may enter test to pull test Data");
             Console.Write("Please Enter an E-mail Address: ");
             input = Console.ReadLine();
 
             if (input == "test")
             {
-                Test(data, reg);
+                Test(reg);
             }
             else
             {
@@ -147,19 +116,20 @@ namespace Regexp
             }
         }
 
-        static void NameVaildation(string[] data)
+        static void NameVaildation()
         {
             Console.Clear();
             string input;
-            Regex single = new Regex(@"^[A-Z][a-z]\w+$");
-            Regex firstlast = new Regex(@"^[A-Z][a-z]\w+\s[A-Z][a-z]\w+$");
+            Regex single = new Regex(@"^[A-Z][a-z]\w{1,30}$");
+            Regex firstlast = new Regex(@"^([A-Z][a-z]\w+\s[A-Z][a-z]\w+)\w{1,30}$");
 
+            Console.WriteLine("You may enter test to pull test Data");
             Console.Write("Please Enter a Name: ");
             input = Console.ReadLine();
 
             if (input == "test")
             {
-                Test(data, single, firstlast);
+                Test(single, firstlast);
             }
             else
             {
@@ -181,11 +151,9 @@ namespace Regexp
                 }
 
             }
-
-                
         }
 
-        static void PhoneValidation(string[] data)
+        static void PhoneValidation()
         {
             Console.Clear();
             string input;
@@ -193,61 +161,84 @@ namespace Regexp
 //            Regex phonedash = new Regex(@"^([0-9]){3}-([0-9]){3}-([0-9]){4}$");
             Regex phone = new Regex(@"^([0-9]){3}[\.,-]([0-9]){3}[\.,-]([0-9]){4}$");
 
-            Console.Write("Please Enter a Name: ");
+            Console.WriteLine("You may enter test to pull test Data");
+            Console.Write("Please Enter a Phone number: ");
             input = Console.ReadLine();
 
             if (input == "test")
             {
-                Test(data, phonepara, phone);
+                Test(phonepara, phone);
+            }
+            else if (phonepara.IsMatch(input) || phone.IsMatch(input))
+            {
+                Console.WriteLine("That is a Valid phone number");
             }
             else
             {
-
+                Console.WriteLine("What ever that is it is not a phone number");
             }
         }
       
 
-        static void DateValidation(string[] data)
+        static void DateValidation()
         {
             Console.Clear();
+            string input;
             bool doesItMatch;
             Regex Date = new Regex(@"^[0-1][0-9]/[0-3][0-9]/[0-1][0-9][0-9][0-9]$");
 
-            for (int i = 0; i < data.Length; i++)
+            Console.WriteLine("You may enter test to pull test Data");
+            Console.Write("Please Enter a Date (mm/dd/yyyy): ");
+            input = Console.ReadLine();
+
+            if (input == "test")
             {
-                doesItMatch = Date.IsMatch(data[i]);
-                Console.Write("{0,30} is a Date? (dd/mm/yyyy format): ", data[i]);
-                Console.WriteLine(doesItMatch);
+                Test(Date);
             }
-            
+            else
+            {
+                doesItMatch = Date.IsMatch(input);
+                if (doesItMatch)
+                {
+                    Console.WriteLine("That is a Valid Date");
+                }
+                else
+                {
+                    Console.WriteLine("That is not a Date");
+                }
+            }
+
+
         }
 
-        static void Test(string[] data, Regex test1)
+        static void Test(Regex test1)
         {
             Console.Clear();
+            string[] data = GetTestData();
             bool doesItMatch;
+            Console.WriteLine();
             for (int i = 0; i < data.Length; i++)
             {
                 doesItMatch = test1.IsMatch(data[i]);
-                Console.Write("{0,30}: ", data[i]);
-                Console.WriteLine(doesItMatch);
+                Console.WriteLine("{0,50}: {1,7}", data[i], doesItMatch);
             }
         }
 
-        static void Test(string[] data, Regex test1, Regex test2)
+        static void Test(Regex test1, Regex test2)
         {
             Console.Clear();
+            string[] data = GetTestData();
             bool[,] doesItMatch = new bool[2,data.Length];
-            Console.WriteLine("Test 1");
             for (int i = 0; i < data.Length; i++)
             {
                 doesItMatch[0,i] = test1.IsMatch(data[i]);
             }
-            Console.WriteLine("Test 2");
             for (int i = 0; i < data.Length; i++)
             {
                 doesItMatch[1,i] = test2.IsMatch(data[i]);
             }
+            Console.WriteLine("If Test 1 or Test 2 is valid the the data is valid");
+            Console.WriteLine("Some additional tests are done in the non test version using if statments");
             Console.WriteLine("{0,40}: {1,7} {2,7}", "data", "Test 1", "Test 2");
             for (int i = 0; i < data.Length; i++)
             {
@@ -255,5 +246,45 @@ namespace Regexp
             }
         }
 
+        static string[] GetTestData()
+        {
+            string[] testdata =
+            {
+                "",
+                "Chuck@TechU..om",
+                "David@Quick@enLoans.com",
+                "Jeff@Grandcircus.com",
+                "madeup@someplace.ca",
+                "fake%new.com",
+                "Jim@Desktop.org",
+                "@place.com",
+                "Bob@Burgerscom",
+                "Keith@Voltron.defender",
+                "David",
+                "Abcdefghijklmn Opqrstuvwxyzextrastuff",
+                "That Guy",
+                "N0t Name",
+                "1/12/2020",
+                "12/31/1999",
+                "15/12/1986",
+                "(1d6)-456-7890",
+                "(123)-456-7890",
+                "1-1-1",
+                "(1234)56-7890",
+                "1&3-456-7890",
+                "9999999999",
+                "123.456.7890",
+                "123-456-7890"
+            };
+            return testdata;
+        }
+
+        // @ before "" = able to make \ as a ignore next charater called a literal as a special charater
+        // ^ = Start of String
+        // . = wildcard
+        // * = any number of previous charater
+        // $ = end of String
+        // [abc] = match the char to only a,b, or c
+        // [^abc] = match the char to any except a,b or c
     }
 }
